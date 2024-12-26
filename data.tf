420448e0665360fd63b69101c07fb7fe5deabf2d
yllee9127 <yllee9127@gmail.com>
2024-12-26 18:01:53 +0800

    Terraform Codes for VPC Peering setup

--- data.tf
+++ data.tf
@@ -1,3 +1,29 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+data "aws_subnets" "public-1" {
+  /*filter {
+    name = "vpc-id"
+    #values = [var.vpc_id]
+    values = module.vpc-1.vpc_id
+  }*/
+
+  filter {
+    name = "tag:Name"
+    values = ["yl-vpc-1-public*"]
+  }
+}
+
+data "aws_subnets" "public-2" {
+  /*filter {
+    name = "vpc-id"
+    # values = [var.vpc_id]
+    values = module.vpc-2.vpc_id
+  }*/
+
+  filter {
+    name = "tag:Name"
+    values = ["yl-vpc-2-public*"]
+  }
+}
--- main.tf
+++ main.tf
@@ -13,16 +13,7 @@ module "vpc-1" {
   single_nat_gateway   = false
   enable_dns_hostnames = true # needed for DNS resolution
 }
-/*
-resource "aws_db_subnet_group" "shared-1" {
-  name       = "yl-subnet-group-1"
-  subnet_ids = module.vpc.public_subnets
 
-  tags = {
-    Name = "yl-subnet-group-1"
-  }
-}
-*/
 module "vpc-2" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.16.0"
@@ -38,14 +29,131 @@ module "vpc-2" {
   single_nat_gateway   = false
   enable_dns_hostnames = true # needed for DNS resolution
 }
-/*
-resource "aws_db_subnet_group" "shared-2" {
-  name       = "yl-subnet-group-2"
-  subnet_ids = module.vpc.public_subnets
+
+
+resource "aws_instance" "web_app_1" {
+  #count = 2
+
+  ami = "ami-04c913012f8977029"
+  #instance_type = var.instance_type
+  instance_type = "t2.micro"
+  for_each = toset(data.aws_subnets.public-1.ids)
+  subnet_id = each.value
+  #subnet_id = data.aws_subnets."public-${count.index+1}".ids[0]
+  #subnet_id = local.selected_subnet_ids[count.index % length(local.selected_subnet_ids)]
+  vpc_security_group_ids = [aws_security_group.allow_tls_vpc_1.id]
+  # user_data = templatefile("init-script.sh")
+
+  depends_on = [aws_security_group.allow_tls_vpc_1]
+
+  #key_name = "yl-key-pair"
+  associate_public_ip_address = true
 
   tags = {
-    Name = "yl-subnet-group-2"
+    Name = "${local.name_prefix}-ec2-1"
   }
 }
-*/
+
+resource "aws_instance" "web_app_2" {
+  #count = 2
+
+  ami = "ami-04c913012f8977029"
+  #instance_type = var.instance_type
+  instance_type = "t2.micro"
+  for_each = toset(data.aws_subnets.public-2.ids)
+  subnet_id = each.value
+  #subnet_id = data.aws_subnets."public-${count.index+1}".ids[0]
+  #subnet_id = local.selected_subnet_ids[count.index % length(local.selected_subnet_ids)]
+  vpc_security_group_ids = [aws_security_group.allow_tls_vpc_2.id]
+  # user_data = templatefile("init-script.sh")
+
+  #key_name = "yl-key-pair"
+  associate_public_ip_address = true
+
+  depends_on = [aws_security_group.allow_tls_vpc_2]
+
+  tags = {
+    Name = "${local.name_prefix}-ec2-2"
+  }
+}
+
+
+locals {
+  name_prefix = "yl"
+}
+
+resource "aws_security_group" "allow_tls_vpc_1" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = module.vpc-1.default_vpc_id
+  vpc_id      = module.vpc-1.vpc_id
+
+  tags = {
+    Name = "allow_tls_vpc_1"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_vpc_1" {
+  security_group_id = aws_security_group.allow_tls_vpc_1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4_vpc_1" {
+  security_group_id = aws_security_group.allow_tls_vpc_1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_vpc_1" {
+  security_group_id = aws_security_group.allow_tls_vpc_1.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_security_group" "allow_tls_vpc_2" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = module.vpc-2.default_vpc_id
+  vpc_id      = module.vpc-2.vpc_id
+
+  tags = {
+    Name = "allow_tls_vpc_2"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4_vpc_2" {
+  security_group_id = aws_security_group.allow_tls_vpc_2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4_vpc_2" {
+  security_group_id = aws_security_group.allow_tls_vpc_2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_vpc_2" {
+  security_group_id = aws_security_group.allow_tls_vpc_2.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 
