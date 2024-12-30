14f0f0b7b09285fd8c671c7c8c031a09bb275e35
yllee9127 <yllee9127@gmail.com>
2024-12-30 11:28:51 +0800

    Updated for route tables

--- data.tf
+++ data.tf
@@ -13,6 +13,7 @@ data "aws_subnets" "public-1" {
     name = "tag:Name"
     values = ["yl-vpc-1-public*"]
   }
+  depends_on = [module.vpc-1]
 }
 
 data "aws_subnets" "public-2" {
@@ -26,4 +27,52 @@ data "aws_subnets" "public-2" {
     name = "tag:Name"
     values = ["yl-vpc-2-public*"]
   }
+    depends_on = [module.vpc-2]
 }
+
+resource "aws_vpc_peering_connection" "yl_vpc_peer" {
+  #peer_owner_id = var.peer_owner_id
+  peer_vpc_id   = module.vpc-2.vpc_id
+  vpc_id        = module.vpc-1.vpc_id
+  auto_accept   = true
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+resource "aws_route" "vpc-1_to_vpc-2" {
+  # ID of VPC 1 main route table.
+  #route_table_id = "${aws_vpc.primary.main_route_table_id}"
+  #route_table_id = module.vpc-1.vpc_main_route_table_id
+  route_table_id = module.vpc-1.public_route_table_ids[0]
+
+  # CIDR block / IP range for VPC 2.
+  #destination_cidr_block = "${aws_vpc.secondary.cidr_block}"
+  destination_cidr_block = module.vpc-2.vpc_cidr_block
+
+  # ID of VPC peering connection.
+  #vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
+  vpc_peering_connection_id = aws_vpc_peering_connection.yl_vpc_peer.id
+}
+
+resource "aws_route" "vpc-2_to_vpc-1" {
+  # ID of VPC 1 main route table.
+  #route_table_id = "${aws_vpc.primary.main_route_table_id}"
+  #route_table_id = module.vpc-2.vpc_main_route_table_id
+  route_table_id = module.vpc-2.public_route_table_ids[0]
+
+  # CIDR block / IP range for VPC 2.
+  #destination_cidr_block = "${aws_vpc.secondary.cidr_block}"
+  destination_cidr_block = module.vpc-1.vpc_cidr_block
+
+  # ID of VPC peering connection.
+  #vpc_peering_connection_id = "${aws_vpc_peering_connection.primary2secondary.id}"
+  vpc_peering_connection_id = aws_vpc_peering_connection.yl_vpc_peer.id
+}
+
+
+
--- main.tf
+++ main.tf
@@ -1,35 +1,3 @@
-module "vpc-1" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.16.0"
-  name    = "yl-vpc-1"
-
-  cidr             = "10.1.0.0/16"
-  azs              = slice(data.aws_availability_zones.available.names, 0, 3)
-  # private_subnets  = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
-  public_subnets   = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
-  # database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
-
-  enable_nat_gateway   = false  # since no private subnet
-  single_nat_gateway   = false
-  enable_dns_hostnames = true # needed for DNS resolution
-}
-
-module "vpc-2" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.16.0"
-  name    = "yl-vpc-2"
-
-  cidr             = "10.2.0.0/16"
-  azs              = slice(data.aws_availability_zones.available.names, 0, 3)
-  # private_subnets  = ["10.2.1.0/24", "10.2.2.0/24", "10.2.3.0/24"]
-  public_subnets   = ["10.2.101.0/24", "10.2.102.0/24", "10.2.103.0/24"]
-  # database_subnets = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
-
-  enable_nat_gateway   = false # since no private subnet
-  single_nat_gateway   = false
-  enable_dns_hostnames = true # needed for DNS resolution
-}
-
 locals {
   name_prefix = "yl"
 }
@@ -48,7 +16,7 @@ resource "aws_instance" "web_app_1" {
   vpc_security_group_ids = [aws_security_group.allow_tls_vpc_1.id]
   user_data = templatefile("init-script.sh", {file_content = "webapp"})
 
-  depends_on = [aws_security_group.allow_tls_vpc_1]
+  depends_on = [module.vpc-1, aws_security_group.allow_tls_vpc_1]
 
   #key_name = "yl-key-pair"
   associate_public_ip_address = true
